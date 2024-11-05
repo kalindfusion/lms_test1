0f63fe090f7211d32253bc8d779e57044a89e3fa
Kalin Ramsey <kalin.ramsey@dfusioninc.com>
2024-11-04 18:49:47 -0800

    init commit

---
+++ pools/247/247.js
@@ -0,0 +1,15 @@
+window.CPPool247Init = function() { cp.model['247Data']={
+};cp.model.data['247']={
+pqs:[],
+gqs:[],
+sqs:[],
+rgqs:[],
+rsqs:[],
+hasCC:false
+};
+cp.poolResources["247Images"]=[];
+cp.poolResources["247Videos"]=[
+];
+cp.poolResources["247SlideVideos"]=[
+];
+}
